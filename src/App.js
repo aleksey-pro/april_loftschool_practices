b5a78190839ebe05a8b093e813a962559cffd1f2
Artem Samofalov <dex157@gmail.com>
2018-04-06 18:41:24 +0300

    add code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
 
-export default () => <p>App</p>;
+
+export class App extends Component {
+  state = {
+    clicks: 0,
+  };
+
+  increment = () => {
+    this.setState(({ clicks }) => ({ clicks: clicks + 1 }));
+  };
+
+  render() {
+    const { clicks } = this.state;
+    return (
+      <div>
+        {Array.from(new Array(10), (_, i) => i).map(i => (
+          <p key={i}>{i}</p>
+        ))}
+        <Counter clicks={clicks} />
+        {clicks < 10 ? (
+          <button onClick={this.increment}>+</button>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+class Counter extends Component {
+  render() {
+    const { clicks } = this.props;
+    return <p>{clicks}</p>;
+  }
+}
+
+export default App;
